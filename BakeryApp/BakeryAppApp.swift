5acc111ca13790c20052d631d0ff56b1bc6589c4
Yeison Henao <60892680+YeisonHenao@users.noreply.github.com>
2024-01-11 10:59:48 -0500

    Initial Commit

---
+++ BakeryApp/BakeryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakeryAppApp.swift
+//  BakeryApp
+//
+//  Created by desarrollador3 on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BakeryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
